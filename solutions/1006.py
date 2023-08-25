b205f5cd981447eb53ba437d1e18c1fd3a750675
Carlos Robert <crobertdmelo@gmail.com>
2023-08-25 14:36:39 -0300

    Add - 1006 solution added

---
+++ solutions/1006.py
@@ -0,0 +1,6 @@
+grade_a = float(input())
+grade_b = float(input())
+grade_c = float(input())
+
+average = ((grade_a * 2.0) + (grade_b * 3.0) + (grade_c * 5.0)) / 10.0
+print(f'MEDIA = {average:.1f}')
