1308b87f7cc2a1ab0aec89e619a722ac897b8a22
Carlos Robert <crobertdmelo@gmail.com>
2023-08-29 16:33:06 -0300

    Add - 1048 solution added

---
+++ solutions/1048.py
@@ -0,0 +1,23 @@
+salary = float(input())
+
+
+def get_increase_salary(percentage: float, salary: float):
+    increase_amount = percentage / 100 * salary
+    new_salary = salary + increase_amount
+    return percentage, increase_amount, new_salary
+
+
+if 0 <= salary <= 400.00:
+    percentage, increase_amount, new_salary = get_increase_salary(15, salary)
+elif 400.01 <= salary <= 800.00:
+    percentage, increase_amount, new_salary = get_increase_salary(12, salary)
+elif 800.01 <= salary <= 1200.00:
+    percentage, increase_amount, new_salary = get_increase_salary(10, salary)
+elif 1200.01 <= salary <= 2000.00:
+    percentage, increase_amount, new_salary = get_increase_salary(7, salary)
+else:
+    percentage, increase_amount, new_salary = get_increase_salary(4, salary)
+
+print(f"Novo salario: {new_salary:.2f}")
+print(f"Reajuste ganho: {increase_amount:.2f}")
+print(f"Em percentual: {percentage} %")
